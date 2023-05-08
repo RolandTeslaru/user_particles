02bd66867bcecee9e1ec84a100fb1f3fb592fcc5
Roland <roland.teslaru@protonmail.com>
2023-05-08 20:24:40 +0300

    transition working

--- src/threejs/scene.ts
+++ src/threejs/scene.ts
@@ -1,20 +1,8 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { GUI } from "dat.gui";
-import gsap from "gsap";
 import { lerp } from "three/src/math/MathUtils";
-import {
-  ModifierStack,
-  Twist,
-  Noise,
-  Cloth,
-  UserDefined,
-  Taper,
-  Break,
-  Bloat,
-  Vector3,
-  ModConstant
-} from "three.modifiers";
+
 import * as TWEEN from '@tweenjs/tween.js'
 
 // Setup scene
@@ -24,27 +12,27 @@ let camera = new THREE.PerspectiveCamera(
   window.innerWidth / window.innerHeight,
   1,
   100000
-);
-let renderer = new THREE.WebGLRenderer();
-let controls = new OrbitControls(camera, renderer.domElement);
-
-renderer.setSize(window.innerWidth, window.innerHeight);
-//@ts-ignore
-document.getElementById("threejsCanvas").appendChild(renderer.domElement);
-console.log(document.getElementById("threejsCanvas"));
-
-function resizeRenderer() {
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
+  );
+  let renderer = new THREE.WebGLRenderer();
+  let controls = new OrbitControls(camera, renderer.domElement);
+  
   renderer.setSize(window.innerWidth, window.innerHeight);
-}
-
+  //@ts-ignore
+  document.getElementById("threejsCanvas").appendChild(renderer.domElement);
+  console.log(document.getElementById("threejsCanvas"));
+  
+  function resizeRenderer() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+  }
+  
 window.addEventListener("resize", resizeRenderer);
 
 controls.enableDamping = true;
 controls.enablePan = false;
 
-scene.background = new THREE.Color(0x000000);
+scene.background = new THREE.Color(0x160016);
 
 controls.update();
 
@@ -178,12 +166,18 @@ const galaxyGeometry = new THREE.BufferGeometry().setFromPoints(galaxyPoints);
 galaxyGeometry.setAttribute("size", new THREE.Float32BufferAttribute(sizes, 1));
 galaxyGeometry.setAttribute("shift", new THREE.Float32BufferAttribute(shift, 4));
 
-const galaxyPositionsAttribute = galaxyGeometry.getAttribute("position") as THREE.Float32BufferAttribute;
+const galaxyPositionsAttribute = new THREE.Float32BufferAttribute( galaxyGeometry.attributes.position.array, 3 );
 
+const particleMaterial = new THREE.PointsMaterial({
+  size: 0.125,
+  depthTest: false,
+  // blending: THREE.AdditiveBlending,
+  sizeAttenuation: true,
+  vertexColors: true,
+});
 const galaxyPointsSystem = new THREE.Points(galaxyGeometry, material);
 scene.add(galaxyPointsSystem);
 
-console.log("galaxy Geometry", galaxyGeometry.getAttribute("position"));
 
 
 
@@ -209,13 +203,6 @@ function getPixelColor(
   return new THREE.Color(r / 255, g / 255, b / 255);
 }
 
-const particleMaterial = new THREE.PointsMaterial({
-  size: 0.125,
-  depthTest: false,
-  // blending: THREE.AdditiveBlending,
-  sizeAttenuation: true,
-  vertexColors: true,
-});
 
 generateImageParticles("/profile387.jpg");
 
@@ -282,81 +269,126 @@ function generateImageParticles(path: string) {
 
     console.log("Morph attribtes image" , galaxyPointsSystem.geometry.morphAttributes.position)
     console.log("Image Postions", targetPositionsAttribute)
-    scene.add(particleSystem);
+    // scene.add(particleSystem);
   };
 }
+
+
+
 
 //                                                                  //                
 //   -----------------  Morph Particles Animation ----------------- //
 //                                                                  //            
 
-let useGalaxyPostions = true;
+let iSGalaxyMode = true;
 
+function setProfileMode(){
+  iSGalaxyMode = false;
+  galaxyPointsSystem.material = particleMaterial; 
+  galaxyPointsSystem.geometry.setAttribute("color", new THREE.BufferAttribute(targetColors, 3));
+}
+function setGalaxyMode(){
+  iSGalaxyMode = true;
+  galaxyPointsSystem.material = material;
+
+
+}
 
 // Update Particles frin initial position to target position
 
-function updateParticleSys(time: any, initialPositionsAttribute: THREE.Float32BufferAttribute, tartgetPostionsAttribute: THREE.Float32BufferAttribute){
-  
+function updateParticleSys(
+  startTime: number, 
+  duration: number, 
+  initialPositionsAttribute: THREE.Float32BufferAttribute, 
+  targetPositionsAttribute: THREE.Float32BufferAttribute,
+  DistanceScale: number = 1
+) {
+  const currentTime = Date.now();
+  const elapsed = currentTime - startTime;
+  const progress = elapsed / duration;
+  const t = Math.min(progress, 1);
 
-  var t = (Math.sin(time) + 1) / 2;
-  console.log(t)
-  var tweens = [];
+  for (let i = 0; i < TOTAL_SIZE; i++) {
+    const initialX = initialPositionsAttribute.getX(i);
+    const initialY = initialPositionsAttribute.getY(i);
+    const initialZ = initialPositionsAttribute.getZ(i);
+    const finalX = targetPositionsAttribute.getX(i);
+    const finalY = targetPositionsAttribute.getY(i);
+    const finalZ = targetPositionsAttribute.getZ(i);
 
-  for(var i = 0 ; i< TOTAL_SIZE; i++){
-    var initialX = initialPositionsAttribute.getX(i);
-    var initialY = initialPositionsAttribute.getY(i);
-    var initialZ = initialPositionsAttribute.getZ(i);
-
-    var finalX = tartgetPostionsAttribute.getX(i);
-    var finalY = tartgetPostionsAttribute.getY(i);
-    var finalZ = tartgetPostionsAttribute.getZ(i);
-
-    var x = initialX * (1 - t) + finalX * t;
-    var y = initialY * (1 - t) + finalY * t;
-    var z = initialZ * (1 - t) + finalZ * t;
+    const x = initialX * (1 - t) + finalX * t / DistanceScale ;
+    const y = initialY * (1 - t) + finalY * t / DistanceScale;
+    const z = initialZ * (1 - t) + finalZ * t / DistanceScale;
 
     galaxyPointsSystem.geometry.attributes.position.setXYZ(i, x, y, z);
-
-   
-    galaxyPointsSystem.geometry.attributes.position.needsUpdate = true;
-
   }
 
-  
+  galaxyPointsSystem.geometry.attributes.position.needsUpdate = true;
 }
 
 const gui = new GUI();
 const morphTrigger = gui.add({ on: false }, "on");
 
+let startTime = 0;
+let isAnimating = false;
 let morph = false;
 morphTrigger.onChange((value) => {
+  isAnimating = true;
+  startTime = Date.now();
   if(value)
-    morph = true;
-
+  {
+    setProfileMode();
+    console.log("Morphing into ", targetPositionsAttribute )
+  }
+  else {
+    setGalaxyMode();
+    console.log("Morphing into galaxy", galaxyPositionsAttribute)
+  }
 });
 
 let clock = new THREE.Clock();
 
-const duration = 0.001;
+const DURATION = 10000;
 
 
 function animate() {
   requestAnimationFrame(animate);
   controls.update();
   renderer.render(scene, camera);
+  
   let t = clock.getElapsedTime() * 0.5;
   gu.time.value = t * Math.PI;
   const time = Date.now() * 0.001;
   imgMaterial.opacity = Math.sin(time * 2.0) * 0.5 + 0.5;
 
 
-  TWEEN.update();
+  const currentTime = Date.now();
+  const elapsed = currentTime - startTime;
 
-  if(morph){
-    var timet= Date.now() * 0.001;
-    var timeElapsed = Math.min(duration, clock.getElapsedTime());
-    var progress = timeElapsed / duration;      
-    updateParticleSys(time, galaxyPositionsAttribute, targetPositionsAttribute);
+  TWEEN.update(); // update the tweens
+
+  if (isAnimating) {
+    if(iSGalaxyMode){
+      updateParticleSys(
+        startTime,  
+        DURATION, 
+        galaxyPointsSystem.geometry.getAttribute("position") as THREE.Float32BufferAttribute, 
+        galaxyPositionsAttribute);
+    }
+    else{
+      updateParticleSys(
+        startTime,  
+        DURATION, 
+        galaxyPointsSystem.geometry.getAttribute("position") as THREE.Float32BufferAttribute, 
+        targetPositionsAttribute,
+        20
+      );
+    }
+
+    if(elapsed >= DURATION / 10)
+    {
+      isAnimating = false;
+    }
   }
 
 }
