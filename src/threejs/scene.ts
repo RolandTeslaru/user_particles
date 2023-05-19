435bca923862123cc32a21bcc1d28f171afc7878
Roland Teslaru <89258659+RolandTeslaru@users.noreply.github.com>
2023-05-19 22:31:15 +0300

    fixed4

--- src/threejs/scene.ts
+++ src/threejs/scene.ts
@@ -1,7 +1,6 @@
 import * as THREE from "three";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
-import { globalResources } from "./GlobalResources";
 import Galaxy from "./Galaxy";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 export var renderTimeSeconds = 0;
 export var rtPlane: any;
